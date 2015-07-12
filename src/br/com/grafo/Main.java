2ea5a964d7e4fd37ee42cdcae9725ad6c9c5bd19
Maxsuel Marreiro <maxsuelmarreiro@gmail.com>
2015-07-12 02:07:10 -0300

    Commit inicial

---
+++ src/br/com/grafo/Main.java
@@ -0,0 +1,16 @@
+package br.com.grafo;
+
+/**
+ *
+ * @author '-Maxsuel
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Hello World.");
+    }
+    
+}
